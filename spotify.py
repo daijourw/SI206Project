6f0e0ddd55e6acc1637207df2674f13ea8723b31
daijourw <daijourw@umich.edu>
2021-11-30 00:00:36 -0500

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -11,6 +11,11 @@ import csv
 #function read csv file and places mvoies into a list
 
 #set up api
+def SpotifySearch(term):
+    parameters = {"id":}
+    response = requests.get("https://api.spotify.com/v1/albums", params = parameters)
+    data = response.json()
+    results = data["results"]
 
 #
 
