f99f2792eb4eb4706c3eef96f92d7af293889f77
daijourw <daijourw@umich.edu>
2021-11-29 23:29:42 -0500

    Create spotify.py

---
+++ spotify.py
@@ -0,0 +1,17 @@
+# SI 206 Final Project
+# Daijour Williams
+#Get data from spotify api and put into database
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import csv
+
+
+#function read csv file and places mvoies into a list
+
+#set up api
+
+#
+
+#
